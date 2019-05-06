fe33646050b85ef618190587db0bbda9f1fbd875
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-06 11:05:55 -0300

    Create 06-05-19.js

---
+++ LogicaProgramacao/Maio/06-05-19.js
@@ -0,0 +1,11 @@
+var time1, time2, v1, v2, e1, e2, d1, d2, pon1, pon2;
+time1 = prompt('Insira o nome do time 1');
+time2 = prompt('Insira o nome do time 2');
+v1 = parseInt(prompt('Insira o número de vitórias do time 1'));
+e1 = parseInt(prompt('Insira o número de empates do time 1'));
+d1 = parseInt(prompt('Insira o número de derrotas do time 1'));
+v2 = parseInt(prompt('Insira o número de vitórias do time 2'));
+e2 = parseInt(prompt('Insira o número de empates do time 2'));
+d2 = parseInt(prompt('Insira o número de derrotas do time 2'));
+pon1 = v1*3 + e1*1 + d1*0;
+pon2 = v2*3 + e2*1 + d2*0;
