875855719f7e5727b809ffb641a70994552c4fc6
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-07 10:36:26 -0300

    Update 06-05-19.js

--- LogicaProgramacao/Maio/06-05-19.js
+++ LogicaProgramacao/Maio/06-05-19.js
@@ -1,24 +1,25 @@
 /* Faça um programa que leia o nome de dois times e o número de vitórias, empates e derrotas.
-Calcule e mostre o time ganhador e a quantidade de pontos que este teve
+Calcule e mostre o time ganhador e a quantidade de pontos que este teve, sendo
+VITÓRIAS = 3 pontos;
 */
 
-var time1, time2, v1, v2, e1, e2, d1, d2, pon1, pon2;
-time1 = prompt('Insira o nome do time 1');
-time2 = prompt('Insira o nome do time 2');
-v1 = parseInt(prompt('Insira o número de vitórias do time 1'));
-e1 = parseInt(prompt('Insira o número de empates do time 1'));
-d1 = parseInt(prompt('Insira o número de derrotas do time 1'));
-v2 = parseInt(prompt('Insira o número de vitórias do time 2'));
-e2 = parseInt(prompt('Insira o número de empates do time 2'));
-d2 = parseInt(prompt('Insira o número de derrotas do time 2'));
-pon1 = v1*3 + e1*1 + d1*0;
-pon2 = v2*3 + e2*1 + d2*0;
-if (pon1 > pon2) {
-  alert('O time '+ time1 +' ganhou!'); 
+var T1, T2, V1, V2, E1, E2, D1, D2, P1, P2;
+T1 = prompt('Insira o nome do time 1');
+T2 = prompt('Insira o nome do time 2');
+V1 = parseInt(prompt('Insira o número de vitórias do '+ T1));
+E1 = parseInt(prompt('Insira o número de empates do '+ T1));
+D1 = parseInt(prompt('Insira o número de derrotas do '+ T1));
+V2 = parseInt(prompt('Insira o número de vitórias do '+ T2));
+E2 = parseInt(prompt('Insira o número de empates do '+ T2));
+D2 = parseInt(prompt('Insira o número de derrotas do '+ T2));
+P1 = V1*3 + E1;
+P2 = V2*3 + E2;
+if (P1 > P2) {
+  alert('O time '+ T1 +' ganhou!'); 
 }
-if (pon1 < pon2) {
-  alert('O time '+ time2 +' ganhou!'); 
+if (P1 < P2) {
+  alert('O time '+ T2 +' ganhou!'); 
 }
-if (pon1 == pon2) {
-  alert(time1 +' e '+ time2 +' empataram com '+ pon1 +' pontos');  
+if (P1 == P2) {
+  alert(T1 +' e '+ T2 +' empataram com '+ P1 +' pontos');  
 }
