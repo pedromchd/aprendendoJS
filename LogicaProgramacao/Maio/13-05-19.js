2135bb2510f0b91925a9363b7cb9de6c9229277f
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-13 11:53:51 -0300

    Update 13-05-19.js

--- LogicaProgramacao/Maio/13-05-19.js
+++ LogicaProgramacao/Maio/13-05-19.js
@@ -58,5 +58,36 @@ s = Math.round(Math.random()*10);
 if (n == s) {
     alert('Você ganhou');
 } else {
-    alert('Você perdeu');
+    alert('Você perdeu, o número era '+ s);
+}
+
+//SORTEIO
+var n, s;
+n = parseInt(prompt('Insira um número inteiro n'));
+s = Math.round(Math.random()*10);
+if (n == s) {
+    alert('Você acertou');
+} else {
+    alert('Você errou');
+    if (s >= 0 && s <= 4) {
+        alert('O número está entre 0 e 4'):
+    }
+    if (s%5 == 0) {
+        alert('O número é múltiplo de 5')
+    }
+    if (s%2 == 1) {
+        alert('O número é ímpar')
+    }
+    if (s == 2 || s == 3 || s == 5 || s == 7)
+        alert('O número é primo')
+    }
+    if (s < 6 || s > 7 || s < 9) {
+        alert('O número é, ou menor que 6 e 9, ou maior que 7') {
+    }
+    n = parseInt(prompt('Segunda tentativa'));
+    if (n == s) {
+        alert('Você acertou');
+    } else {
+        alert('Você errou, o número era '+ s);
+    }
 }
