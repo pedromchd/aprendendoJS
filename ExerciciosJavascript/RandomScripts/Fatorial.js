cabebf35d184bb860000f431648d14b8ed5344d4
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-12 22:16:55 -0300

    Update Fatorial.js

--- ExerciciosJavascript/RandomScripts/Fatorial.js
+++ ExerciciosJavascript/RandomScripts/Fatorial.js
@@ -12,3 +12,12 @@ if (num >= 1) {
     fat = (num == 0) ? 1: "NaN";    
     alert(fat);
 }
+
+
+var n, f;
+n = parseInt(prompt('n'));
+f = (n >= 0) ? 1: 'NaN';
+for (n; n>=1; n--) { 
+  f *= n;
+}
+alert(f);
