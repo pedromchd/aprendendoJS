b9ff56a39b87a112789d783a4ab379d3cd767988
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-13 19:54:38 -0300

    Update Calculadora.js

--- ExerciciosJavascript/Calculadora.js
+++ ExerciciosJavascript/Calculadora.js
@@ -6,15 +6,15 @@ n2 = parseFloat(prompt('Insira outro número'));
 alert(eval(n1+op+n2));
 
 //CALCULADORA + - * / ** rt %
-var n, n1, n2, op;
+var n1, n2, op;
 n1 = parseFloat(prompt('Insira um número'));
 op = prompt('Insira a operação + - * / ** rt %');
 if (op == '**' || op == 'rt') {
-    n = parseInt(prompt('Insira o índice/expoente'));
+    n2 = parseInt(prompt('Insira o índice/expoente'));
     if (op == '**') {
-        alert(n1**n);
+        alert(n1**n2);
     } else {
-        alert(n1**(1/n));
+        alert(n1**(1/n2));
     }
 } else {
     n2 = parseFloat(prompt('Insira outro número'));
