804b27770195efde53d8c075d3b4ae77b859222c
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-13 11:05:59 -0300

    Update 13-05-19.js

--- LogicaProgramacao/Maio/13-05-19.js
+++ LogicaProgramacao/Maio/13-05-19.js
@@ -26,3 +26,34 @@ n1 = parseFloat(prompt('Insira um número'));
 op = prompt('Insira a operação: + - * /');
 n2 = parseFloat(prompt('Insira outro número'));
 alert(eval(n1+op+n2));
+
+
+//CALCULADORA + - * / ** rt %
+var n, n1, n2, op, res;
+n1 = parseFloat(prompt('Insira um número'));
+op = prompt('Insira a operação: + - * / ** rt %');
+if (op == '**') {
+    n = parseInt(prompt('Insira o expoente'));    
+    res = n1**n;
+} else if (op == 'rt') {
+    n = parseInt(prompt('Insira o índice'));
+    res = n1**(1/n)
+} else {
+    n2 = parseFloat(prompt('Insira outro número'));
+}
+if (op == '+') {
+    res = n1 + n2;
+}
+if (op == '-') {
+    res = n1 - n2;
+}
+if (op == '*') {
+    res = n1 * n2;
+}
+if (op == '/') {
+    res = n1 / n2;
+}
+if (op == '%') {
+    res = n1 % n2;
+}
+alert(res);
