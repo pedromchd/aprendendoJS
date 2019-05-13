17d9166f87ac4ec8f082504353eab8d282c25476
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-13 12:03:08 -0300

    Update 13-05-19.js

--- LogicaProgramacao/Maio/13-05-19.js
+++ LogicaProgramacao/Maio/13-05-19.js
@@ -70,24 +70,57 @@ if (n == s) {
 } else {
     alert('Você errou');
     if (s >= 0 && s <= 4) {
-        alert('O número está entre 0 e 4')
-    }
-    if (s%5 == 0) {
-        alert('O número é múltiplo de 5')
-    }
-    if (s%2 == 1) {
-        alert('O número é ímpar')
-    }
-    if (s == 2 || s == 3 || s == 5 || s == 7) {
-        alert('O número é primo')
-    }
-    if (s < 6 || s > 7 || s < 9) {
-        alert('O número é, ou menor que 6 e 9, ou maior que 7')
+        alert('O número está entre 0 e 4');
+    } else if (s%5 == 0) {
+        alert('O número é múltiplo de 5');
+    } else if (s%2 == 1) {
+        alert('O número é ímpar');
+    } else if (s == 2 || s == 3 || s == 5 || s == 7) {
+        alert('O número é primo');
+    } else if (s < 6 || s > 7 || s < 9) {
+        alert('O número é, ou menor que 6 e 9, ou maior que 7');
     }
     n = parseInt(prompt('Segunda tentativa'));
     if (n == s) {
         alert('Você acertou');
     } else {
         alert('Você errou, o número era '+ s);
+    }
+}
+
+//SORTEIO
+var n, s;
+n = parseInt(prompt('Insira um número inteiro n'));
+l = parseInt(prompt('Insira um límite de números a serem sorteados'));
+s = Math.round(Math.random()*l);
+if (n == s) {
+    alert('Você acertou');
+} else {
+    alert('Você errou');
+    if (s >= 0 && s <= 4) {
+        alert('O número está entre 0 e 4')
+    } else if (s%2 == 1) {
+        alert('O número é ímpar')
+    } else if (s < 6 || s > 7 || s < 9) {
+        alert('O número é, ou menor que 6 e 9, ou maior que 7')
+    }
+    n = parseInt(prompt('Segunda tentativa'));
+    if (n == s) {
+        alert('Você acertou');
+    } else {
+        alert('Você errou de novo');
+        if (s%5 == 0) {
+            alert('O número é múltiplo de 5');
+        } else if (s == 2 || s == 3 || s == 5 || s == 7) {
+            alert('O número é primo');
+        } else if (s%2 == 0) {
+            alert('O número é par');
+        }
+        n = parseInt(prompt('Terceira tentativa'));
+        if (n == s) {
+            alert('Você acertou');
+        } else {
+            alert('Você errou, o número era '+ s);
+        }
     }
 }
