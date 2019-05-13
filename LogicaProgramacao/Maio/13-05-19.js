d513c536f56e1bd64c677d9a1ae31544bca16bda
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-13 11:19:30 -0300

    Update 13-05-19.js

--- LogicaProgramacao/Maio/13-05-19.js
+++ LogicaProgramacao/Maio/13-05-19.js
@@ -57,3 +57,20 @@ if (op == '%') {
     res = n1 % n2;
 }
 alert(res);
+
+
+//CALCULADORA + - * / ** rt %
+var n, n1, n2, op;
+n1 = parseFloat(prompt('Insira um número'));
+op = prompt('Insira a operação + - * / ** rt %');
+if (op == '**' || op == 'rt') {
+    n = parseInt(prompt('Insira o índice/expoente'));
+    if (op == '**') {
+        alert(n1**n);
+    } else {
+        alert(n1**(1/n));
+    }
+} else {
+    n2 = parseFloat(prompt('Insira outro número'));
+    alert(eval(n1+op+n2));
+}
