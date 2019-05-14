d0c235984cf5fe6032193cbe2e02d31e71617570
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-14 10:56:19 -0300

    Update 13-05-19.js

--- LogicaProgramacao/Maio/13-05-19.js
+++ LogicaProgramacao/Maio/13-05-19.js
@@ -52,6 +52,48 @@ if (op == '%') {
 alert(res);
 
 
+//CALCULADORA v3
+var n1, n2, op, res;
+n1 = parseFloat(prompt('Insira um número'));
+op = prompt('Insira a operação [+] [-] [*] [/] [**] [rt] [%]');
+if (op == 'trunc' || op == 'pos') {
+  n1 = parseInt(n1);
+  n2 = parseInt(prompt('Insira as casas a truncar/a posição a se achar'));
+  if (op == 'trunc') {
+    res = n1/(10**n2);
+  } else {
+    n1 /= Math.pow(10,n2);
+    n2 -= parseInt(n1);
+    res = parseInt(n2*10);
+  } 
+} else if (op == '**' || op == 'rt') {
+  n2 = parseInt(prompt('Insira o expoente/índice'));
+  if (op == '**') {
+    res = n1**n2;
+  } else {
+    res = n1**(1/n2);
+  }
+} else {
+  n2 = parseFloat(prompt('Insira outro número'));
+}
+if (op == '+') {
+	res = n1 + n2;
+}
+if (op == '-') {
+	res = n1 - n2;
+}
+if (op == '*') {
+	res = n1 * n2;
+}
+if (op == '/') {
+	res = n1 / n2;
+}
+if (op == '%') {
+	res = n1 % n2;
+}
+alert(res);
+
+
 //SORTEIO v1
 var n, s;
 n = parseInt(prompt('Insira um número inteiro n'));
@@ -61,6 +103,7 @@ if (n == s) {
 } else {
     alert('Você perdeu, o número era '+ s);
 }
+
 
 //SORTEIO v2
 var n, s;
@@ -84,6 +127,7 @@ if (n == s) {
 		alert('Você errou, o número era '+ s);
 	}
 }
+
 
 //SORTEIO v3
 var n, s;
