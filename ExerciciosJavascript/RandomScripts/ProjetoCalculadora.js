640ba86ac52e6b7d6d8739fcccef8e1d4360345c
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-15 18:40:13 -0300

    Update ProjetoCalculadora.js

--- ExerciciosJavascript/RandomScripts/ProjetoCalculadora.js
+++ ExerciciosJavascript/RandomScripts/ProjetoCalculadora.js
@@ -1,43 +1,81 @@
-//[+][-][*][/][%][**]
+//[+][-][*][/][%]
 var n1, n2, op, res;
-n1 = parseInt(prompt('Insira n1));
-op = prompt('Operação [+][-][*][/][%][**]'));
-n2 = parseInt(prompt('Insira n2));
-res = eval(n1+op+n2);
+n1 = parseFloat(prompt('Insira n1'));
+op = prompt('Operação [+][-][*][/][%]');
+n2 = parseFloat(prompt('Insira n2'));
+res = eval(n1 + op + n2);
 alert(res);
 
+//[**]
+n1 = parseFloat(prompt('Insira n1'));
+n2 = parseInt(prompt('Insira o expoente'));
+res = n1**n2;
+alert(res);
+
+
 //Raiz
+n1 = parseFloat(prompt('Insira n1'));
+n2 = parseInt(prompt('Insira o índice'));
+res = n1**(1/n2);
+alert(res);
 
 //Truncar
+n1 = parseInt(prompt('Insira n1'));
+n2 = parseInt(prompt('Insira casas a truncar'));
+res = n1/(10**n2);
+alert(res);
 
 //Posição
+var n1, n2;
+n1 = parseInt(prompt('Insira um número'));
+n2 = parseInt(prompt('Insira a posição'));
+n1 /= 10**n2;
+n1 -= parseInt(n1);
+res = parseInt(n1*10);
+alert(res);
 
 //ListaCircular
+var n1, n2, a, p, res;
+n1 = parseInt(prompt('Insira o tamanho da lista'));
+n2 = parseInt(prompt('Insira a posição'));
+a = (n2-1+n1)%n1;
+p = (n2+1)%n1;
+res = a+' e '+p;
+alert(res);
 
 //AnteriorPróximoMúltiplo
+var n1, n2, a, p, res;
+n1 = parseInt(prompt('Insira n1'));
+n2 = parseInt(prompt('Número de referência'));
+a = n1-1-(n1-1)%n2;
+p = n1+(n2-n1%n2);
+res = a+' e '+p;
+alert(res);
 
 //Sortear
-var n, l;
-l = parseInt(prompt('
+var n1, res;
+n1 = parseInt(prompt('Números sorteados'));
+res = Math.round(Math.random()*n1);
+alert(res);
 
 //Inversão
-var n, a, t, r;
-n = parseInt(prompt('Insira um número'));
-a = parseInt(prompt(n +'\nQuantidade de algarismos'));
+var n1, n2, t, res;
+n1 = parseInt(prompt('Insira um número'));
+n2 = parseInt(prompt(n1 +'\nQuantidade de algarismos'));
 res = '';
-while (a > 0) {
-  a--;
-  t = parseInt(n / 10**a);
-  n -= t * 10**a;
+while (n2 > 0) {
+  n2--;
+  t = parseInt(n1 / 10**n2);
+  n1 -= t * 10**n2;
   res = t + res;
 }
 alert(res);
 
 //Fatorial
-var n, f;
-n = parseInt(prompt('Insira n'));
-f = (n >= 0) ? 1: 'NaN';
-for (n; n > 1; n--) {
-  f *= n;
+var n1, res;
+n1 = parseInt(prompt('Insira n1'));
+res = (n1 >= 0) ? 1: 'NaN';
+for (n1; n1>1; n1--) {
+  res *= n1;
 }
-alert(f);
+alert(res);
