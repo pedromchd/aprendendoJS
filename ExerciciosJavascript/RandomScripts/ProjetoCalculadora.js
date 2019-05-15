44b1a162537bc40e3d8836e5592d9dc458fef3b6
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-15 13:57:00 -0300

    Create ProjetoCalculadora.js

---
+++ ExerciciosJavascript/RandomScripts/ProjetoCalculadora.js
@@ -0,0 +1,30 @@
+//[+][-][*][/][%][**]
+var n1, n2, op, res;
+n1 = parseInt(prompt('Insira n1));
+op = prompt('Operação [+][-][*][/][%][**]'));
+n2 = parseInt(prompt('Insira n2));
+res = eval(n1+op+n2);
+alert(res);
+
+//Raiz
+
+//Truncar
+
+//Posição
+
+//ListaCircular
+
+//AnteriorPróximoMúltiplo
+
+//Inversão
+var n, a, t, r;
+n = parseInt(prompt('Insira um número'));
+a = parseInt(prompt(n +'\nQuantidade de algarismos'));
+res = '';
+while (a > 0) {
+  a--;
+  t = parseInt(n / 10**a);
+  n -= t * 10**a;
+  res = t + res;
+}
+alert(res);
