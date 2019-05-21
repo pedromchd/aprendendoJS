b401836ff66b40fc6fee3c07b965c3cf43890191
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-21 10:38:29 -0300

    Update 21-05-19.js

--- LogicaProgramacao/Maio/21-05-19.js
+++ LogicaProgramacao/Maio/21-05-19.js
@@ -1,2 +1,7 @@
 alert('Oi! Tudo bem?');
 alert('Oi!\nTudo bem?');
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+Criar um programa que informe a quantidade total de calorias de uma refeição 
+a partir das informações do usuário sobre o prato, a sobremesa e a bebida
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
