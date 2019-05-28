4bf1f9d9f09d9dbc5958a47d230a16405f4b483b
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-05-28 11:18:21 -0300

    Update 28-05-19.js

--- LogicaProgramacao/Maio/28-05-19.js
+++ LogicaProgramacao/Maio/28-05-19.js
@@ -1,5 +1,22 @@
+/******************** *
+var C,A;
+scanf("%d%d","C","A");  
+A = Math.ceil((A+1)/C);
+printf("%d",A);
+* ********************/
+
 //TELEFÉRICO
 var C, A;
 C = parseInt(prompt('Insira a capacidade da cabine'));
-A = parseInt(prompt('Insira a quantidade de alunos'));;
+A = parseInt(prompt('Insira a quantidade de alunos'));
 alert('Serão necessárias no mínimo ' + Math.ceil(++A/C));
+
+//XADREZ
+var L, C;
+L = parseInt(prompt('Número de linhas'));
+C = parseInt(prompt('Número de colunas'));
+if (L%2 == C%2) {
+  alert('Branco');
+} else {
+  alert('Preto');
+}
