0605326b71e0c0359d4da77cd73e2dac52a114a1
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-06-24 10:21:01 -0300

    Update 18-06-19.js

--- LogicaProgramacao/Junho/18-06-19.js
+++ LogicaProgramacao/Junho/18-06-19.js
@@ -18,10 +18,10 @@ S += N;
 SQ += N**2;
 C--;
 while (C > 0) {
-	N = parseInt(prompt('Insira outro número'));
-	S += N;
-	SQ += N**2;
-	C--;
+  N = parseInt(prompt('Insira outro número'));
+  S += N;
+  SQ += N**2;
+  C--;
 }
 M = S/M;
 console.log(S,SQ,M);
@@ -38,8 +38,8 @@ var X, Fx;
 X = parseFloat(prompt('Insira um número para x em F(x)=x²-5x+6'));
 while (X != 0) {
   Fx = X**2 - 5*X + 6;
-	alert(Fx);
-	X = parseFloat(prompt('Insira um número para X em F(x)=x²-5x+6'));
+  alert(Fx);
+  X = parseFloat(prompt('Insira um número para X em F(x)=x²-5x+6'));
 }
 
 //5//
@@ -47,12 +47,12 @@ var NuM, NoM, PcM, QnM, Fat, FTo;
 FTo = 0;
 NuM = parseInt(prompt('Número de mercadorias comercializadas'));
 while (NuM > 0) {
-	NoM = prompt('Nome da mercadoria:');
-	PcM = parseFloat(prompt('Preço unitário da mercadoria:'));
-	QnM = parseInt(prompt('Número de ' + NoM + ' vendidas:'));
-	Fat = PcM * QnM;
-	FTo += Fat;
-	NuM--;
+  NoM = prompt('Nome da mercadoria:');
+  PcM = parseFloat(prompt('Preço unitário da mercadoria:'));
+  QnM = parseInt(prompt('Número de ' + NoM + ' vendidas:'));
+  Fat = PcM * QnM;
+  FTo += Fat;
+  NuM--;
 }
 alert(FTo);
 
@@ -60,7 +60,7 @@ alert(FTo);
 var C;
 C = 15;
 while (C <= 200) {
-	alert(C++**2);
+  alert(C++**2);
 }
 
 //7//
@@ -68,16 +68,16 @@ var N, C, T;
 N = parseInt(prompt('Número a ser multiplicado'));
 C = 1;
 while (C <= 10) {
-	alert(N + ' x ' + C + ' = ' + N*C);
-	C++;
+  alert(N + ' x ' + C + ' = ' + N*C);
+  C++;
 }
 
 //8//
 var C, S;
 C = 1; S = 0;
 while (C <= 500) {
-	if (C%2 == 0) S += C;
-	C++;
+  if (C%2 == 0) S += C;
+  C++;
 }
 alert(S);
 
@@ -85,15 +85,15 @@ alert(S);
 var C, I;
 C = 0; I = '';
 while (C <= 20) {
-	if (C%2 != 0) I += C + '\n';
-	C++;
+  if (C%2 != 0) I += C + '\n';
+  C++;
 }
 alert(I);
 
 //10//
 var P = 0;
 while (P <= 15) {
-	alert(3**P++);
+  alert(3**P++);
 }
 
 //11//
@@ -101,8 +101,8 @@ var N, F;
 N = parseInt(prompt("Digite um número"));
 F = (N >= 0) ? 1: 'NaN';
 while (N > 1) {
-	F *= N;
-	N--;
+  F *= N;
+  N--;
 }
 alert(F);
 
@@ -111,6 +111,6 @@ var H, N;
 H = 0;
 N = parseInt(prompt('Número de N de divisões'));
 while (N > 0) {
-	H += 1/N--;
+  H += 1/N--;
 }
 alert(H);
