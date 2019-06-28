7dd6bce2807a45a4a1ded260fb1a48b62a4b3d7e
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-06-28 13:21:49 -0300

    Update

--- ExerciciosJavascript/RandomScripts/Binário.js
+++ ExerciciosJavascript/RandomScripts/Binário.js
@@ -1,16 +1,3 @@
-var N, B, M, D;
-N = parseInt(prompt('Número para converter em binário:'));
-M = N % 2;
-D = parseInt(N / 2);
-B = ''; B = M + B;
-do {
-  M = D % 2;
-  D = parseInt(D / 2);
-  B = M + B;
-} while(D != 0);
-alert(N + ' = ' + B);
-
-
 var N, B, M;
 B = '';
 N = parseInt(prompt('Número para converter em binário:'));
