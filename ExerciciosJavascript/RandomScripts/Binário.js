3b4afd814bffba14f97effdd7a82c595260a103f
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-06-28 22:33:35 -0300

    Update Binário.js

--- ExerciciosJavascript/RandomScripts/Binário.js
+++ ExerciciosJavascript/RandomScripts/Binário.js
@@ -1,3 +1,4 @@
+//CONVERTER INTEIRO EM BINÁRIO
 var N, B, M;
 B = '';
 N = parseInt(prompt('Número para converter em binário:'));
@@ -5,5 +6,18 @@ do {
   M = N%2;
   N = parseInt(N/2);
   B = M + B;
-} while(N != 0);
+} while (N != 0);
 alert(B);
+
+/*CONVERTER BINÁRIO EM INTEIRO
+var N, L, B, I;
+N = prompt('Número para converter em binário');
+L = N.length;
+I = 0;
+while (L > 0) {
+  B = parseInt(N/10**--L);
+  I += B*2**L;
+  N -= B*10**L--;
+}
+alert(I);
+*/
