1f320a9830b1ed76f509d12e5e6d4fa485c20258
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-06-28 21:54:42 -0300

    Delete SorteioEntre2.js

--- ExerciciosJavascript/RandomScripts/SorteioEntre2.js
+++
@@ -1,5 +0,0 @@
-var M, m, S;
-m = parseInt(prompt('Insira o menor número do sorteio'));
-M = parseInt(prompt('Insira o maior número do sorteio'));
-S = Math.round(Math.random()*(M - m) + m);
-console.log(S);
