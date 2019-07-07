e6356dc5c584e0e567019b31f59168b8c9a53a89
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-07-07 11:56:01 -0300

    Update Primo.js

--- ExerciciosJavascript/RandomScripts/Primo.js
+++ ExerciciosJavascript/RandomScripts/Primo.js
@@ -10,3 +10,50 @@ if (P == N+1) {
 } else {
     alert('Não é primo');
 }
+
+var Q, N, P, C;
+/*
+Q = 7
+
+N = 1 N++
+
+C = N C > 0 C--
+D += N/C
+
+D == N+1 P = N + ' ' break;
+
+*/
+
+var Q, N, P, C;
+Q = parseInt(prompt('Mostrar quantos números primos?'));
+for (P = '', N = 1; Q > 0; Q--, N++) {
+	for ( ; ;N++) {
+		for (D = 0, C = N; C > 0; C--) {
+			if (N/C == parseInt(N/C)) {
+				D += N/C;
+			}
+		}
+		if (D == N+1) {
+			P += N + ' '; 
+			break;
+		} 
+	}
+}
+alert(P);
+
+var Q, N, P, C;
+Q = parseInt(prompt('Mostrar quantos números primos?'));
+for (P = '', N = 1; Q > 0; N++) {
+	for (D = 0, C = N; C > 0; C--) {
+		if (N/C == parseInt(N/C)) {
+			D += N/C;
+		}
+	}
+	if (D == N+1) {
+		P += N + ' ';
+		Q--;
+	} 
+}
+alert(P);
+
+var Q, N, P, C, D;
