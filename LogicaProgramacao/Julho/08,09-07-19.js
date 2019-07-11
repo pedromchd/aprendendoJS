3a1a8fcdb3224123c02d406e8ddb2de6fdc416b1
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-07-11 13:31:28 -0300

    Update 08,09-07-19.js

--- LogicaProgramacao/Julho/08,09-07-19.js
+++ LogicaProgramacao/Julho/08,09-07-19.js
@@ -7,7 +7,9 @@ Logica Program. - 2o Bim
 
 //1//
 var M, N, P, C, D;
-M = parseInt(prompt('Insira um número e serão mostrados os números primos até ele'));
+do {
+  M = parseInt(prompt('Insira um número e serão mostrados os números primos até ele'));
+} while (isNaN(M) || M < 0);
 for (P = '', N = 1; N <= M; N++) {
   for (D = 0, C = N; C > 0; C--) {
     if (N/C == parseInt(N/C)) {
@@ -22,9 +24,11 @@ alert('Os números primos até ' + M + ' são:\n' + P);
 
 
 //2//
-var N, D, PRQ
-N = parseInt(prompt('Insira um número e descubra sua pseudo raiz quadrada'));
-for (D = N; D > 0; D--) {
+var N, D, PRQ;
+do {
+  N = parseInt(prompt('Insira um número e descubra sua pseudo raiz quadrada'));
+} while (isNaN(N) || N < 0);
+for (PRQ = 0, D = N; D > 0; D--) {
   if (N/D == parseInt(N/D)) {
     if (D**2 <= N) {
       PRQ = D;
@@ -36,10 +40,21 @@ alert('A PRQ de ' + N + ' é:\n' + PRQ);
 
 
 //3//
-var N, E, P;
-N = parseInt(prompt('Insira um número base da sua potência'));
-E = parseInt(prompt('Insira um expoente para elevar a base'));
-for (P = 1; E > 0; E--) {
+var N, E, P, e;
+do {
+  N = parseInt(prompt('Insira um número base da sua potência'));
+} while (isNaN(N));
+do {
+  E = parseInt(prompt('Insira um expoente para elevar a base'));
+} while (isNaN(E));
+for (e = E, P = 1; E > 0; E--) {
   P *= N;
 }
-alert('O resultado é:\n' + P);
+if (E < 0) {
+  E *= -1;
+  for (P = 1; E > 0; E--) {
+    P *= N;
+  }
+  P = '1/' + P;
+}
+alert('O resultado de ' + N + '^' + e + ' é:\n' + P);
