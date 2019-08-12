84f42a06c02c37fe0767abfb46f383d9d292d6a9
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-08-12 11:40:02 -0300

    Update 12-28-19.js

--- LogicaProgramacao/Agosto/12-28-19.js
+++ LogicaProgramacao/Agosto/12-28-19.js
@@ -1,5 +1,13 @@
 //PJ 2018 F3 PULO
-var C, c;
-C = parseInt(prompt('Número de lajotas:'));
-for (c = C; c > 0; c--) {
-  
+var N, C, L, H;
+N = parseInt(prompt('Número de lajotas:'));
+for (C = N, L = ''; C > 0; C--) {
+  L += prompt('1 para preto, 0 para branco \n' + L);
+}
+for (H = 0; H < L.length; H++) {
+  if (L[H] == '0') {
+    if (L[H+1] == '0') {
+      alert(-1);
+    }
+  }
+}
