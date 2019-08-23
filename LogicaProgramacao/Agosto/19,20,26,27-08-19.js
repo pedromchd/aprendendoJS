74283a74935b40109bb4820d15e5bac1e00bbf6d
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-08-23 12:48:21 -0300

    Update 19,20,26,27-08-19.js

--- LogicaProgramacao/Agosto/19,20,26,27-08-19.js
+++ LogicaProgramacao/Agosto/19,20,26,27-08-19.js
@@ -4,15 +4,17 @@ Logica de Program 3o Bim
 Profa. Raquel M. Barbosa
 ***********************/
 
-var DF, NL, P4, P5, P6, P7, SO, HA, PS, TA, TE, PE, LE, CE, CH, OC, FI, LI, VD, TR, UP, PC, LC, DC, TV;
+var DF, NL, P4, P5, P6, P7, SO, EE, PS, TA, TE, PE, LE, CE, CH, OC, FI, LI, VD, TR, UP, PC, LC, DC, TV, PI, ES;
 do {
   do {
-    DF = prompt('DESCUBRA A PALAVRA... \nENTER - Jogar \nR - Regras \nClique em CANCELAR para sair do jogo');
+    DF = prompt('DESCUBRA A PALAVRA... \nENTER - Jogar \nR - Regras \nC - Créditos \nClique em CANCELAR para sair do jogo');
     if (DF == null) {
       break;
     }
     if (DF.toUpperCase() == 'R') {
       alert('REGRAS: \nNo jogo Descubra a Palavra, você escolhe a quantidade de letras com que deseja jogar e a quantidade de tentativas relativa à dificuldade. \nDepois, deve adivinhar a palavra sorteada pelo computador, seguindo as orientações dadas.');
+    } else if (DF.toUpperCase() == 'C') {
+      alert('DESCUBRA A PALAVRA... \nDesenvolvido por Pedro Garcia Machado \nVersão 1.0 de agosto de 2019');
     }
   } while (DF != '');
   if (DF == null) {
@@ -31,7 +33,7 @@ do {
   } while (NL != 4 && NL != 5 && NL != 6 && NL != 7);
   do {
     do {
-      DF = parseInt(prompt('DIFICULDADE: \n1 - FÁCIL (15 tentativas) \n2 - MÉDIO (10 tentativas) \n3 - DIFÍCIL (8 tentativas)'));
+      DF = parseInt(prompt('DIFICULDADE: \n1 - Fácil (15 tentativas) \n2 - Médio (10 tentativas) \n3 - Difícil (8 tentativas)'));
     } while (DF != 1 && DF != 2 && DF != 3);
     switch (DF) {
       case 1:
@@ -44,28 +46,44 @@ do {
       DF = 8;
     }
   } while (DF != 15 && DF != 10 && DF != 8);
-  alert('LETRAS COM ACENTO E C-CEDILHA NÃO SÃO PERMITIDAS');
-  P4 = '-agua-arco-bala-bote-casa-cola-dado-dote-eixo-erro-faca-frio-gado-grau-halo-hoje-ilha-item-jaca-jogo-lago-lobo-mole-mofo-neto-nove-oleo-olho-pato-peru';
-  P5 = '-ajuda-anexo-barco-beijo-carne-cisne-diabo-doido-esqui-etnia-forca-fruta-golpe-grama-honra-hotel-icone-ideia-jaula-jovem-leite-limbo-magia-motim-navio-nivel-ontem-opera-poder-prato';
-  P6 = '-amanha-anciao-banana-batata-cheiro-contra-destro-digito-escopo-enxame-fossil-fresta-gloria-grande-habito-hafnio-imagem-insumo-jacare-jiboia-laguna-leitor-macula-motriz-nectar-nucleo-objeto-origem-plebeu-prisma';
-  P7 = '-alegria-atitude-bolacha-bussola-capital-chacota-deboche-deserto-empatia-exotico-familia-fissura-gingado-governo-heresia-hortela-iconico-iguaria-jangada-jornada-lampejo-luxuria-maquina-mosaico-natural-nobreza-obliquo-ousadia-planeta-precoce';
-  SO = Math.ceil(Math.random()*30);
-  for (HA = -(++NL); SO > 0; SO--) {
-    HA += NL;
+  alert('LETRAS COM ACENTO E C-CEDILHA NÃO SÃO PERMITIDAS \n\nDigite S para sugestão de palavra \n\nA cada 7 jogadas o jogo mostra uma letra');
+  P4 = ' agua alvo arco auge aula bala bola bolo bota bote caos casa cedo cena cola dado dica dona dote duna eixo egua elmo erro erva faca fato flor frio fuga gado gato gelo grau gula halo hexa hino hoje humo iate iglu ilha irma item jaca jazz jogo joia juri lago laje leve lobo luxo mana meta mito mole mofo nata neto nojo nome nove ogro oleo olho onda ouro pata pato pena peru povo';
+  P5 = ' ajuda anexo barco beijo carne cisne diabo doido esqui etnia forca fruta golpe grama honra hotel icone ideia jaula jovem leite limbo magia motim navio nivel ontem opera poder prato';
+  P6 = ' amanha anciao baleia beleza cheiro contra destro digito escopo enxame fossil fresta gloria grande habito hafnio imagem insumo jacare jiboia laguna leitor macula motriz nectar nucleo objeto origem plebeu prisma';
+  P7 = ' alegria atitude bolacha bussola capital chacota deboche deserto empatia exotico familia fissura gingado governo heresia hortela iguaria ilhota jangada jornada lampejo luxuria maquina mosaico natural nobreza obliquo ousadia planeta precoce';
+  switch (NL) {
+    case 4:
+    PI = P4;
+    break;
+    case 5:
+    PI = P5;
+    break;
+    case 6:
+    PI = P6;
+    break;
+    case 7:
+    PI = P7;
+  }
+  for (ES = 0; PI.indexOf(' ') != -1; ES++) {
+    PI = PI.replace(' ','-');
+  }
+  SO = Math.ceil(Math.random()*ES);
+  for (EE = -(++NL); SO > 0; SO--) {
+    EE += NL;
   }
   NL--;
   switch (NL) {
     case 4:
-    PS = P4.substr(++HA,NL);
+    PS = P4.substr(++EE,NL);
     break;
     case 5:
-    PS = P5.substr(++HA,NL);
+    PS = P5.substr(++EE,NL);
     break;
     case 6:
-    PS = P6.substr(++HA,NL);
+    PS = P6.substr(++EE,NL);
     break;
     case 7:
-    PS = P7.substr(++HA,NL);
+    PS = P7.substr(++EE,NL);
   }
   console.log(PS);
   for (TV = '', PC = 0; PC < NL; PC++) {
@@ -96,7 +114,7 @@ do {
       }
     }
     TA += TE + ') ' + PE + ' - ' + LE + ' letra(s) errada(s) \n';
-    if (TE%5 == 0) {
+    if ((TE+1)%7 == 0) {
       DC = Math.floor(Math.random()*LC.length);
       UP = UP.replace(LC[DC],LC[DC].toUpperCase());
       TA += 'DICA: A palavra possui a letra ' + LC[DC].toUpperCase() + '\n';
@@ -125,6 +143,7 @@ do {
     DF = prompt('Deseja jogar novamente? S/N').toUpperCase();
   } while (DF != 'S' && DF != 'N');
 } while (DF != 'N');
+alert('DESCUBRA A PALAVRA... \nDesenvolvido por Pedro Garcia Machado \nVersão 1.0 de agosto de 2019 \nMUITO OBRIGADO POR JOGAR!!');
 
 /*
 DF - Dificuldade
@@ -134,7 +153,7 @@ P5 - Palavras de 5 letras
 P6 - Palavras de 6 letras
 P7 - Palavras de 7 letras
 SO - Sorteador
-HA - Hífem at
+EE - Espaço em
 PS - Palavra sorteada
 TA - Tentativa anterior
 TE - Tentativa
@@ -152,11 +171,11 @@ PC - Percorredor de character
 LC - Lower case
 DC - Dica
 TV - Traçejado verificador
+PI - Palavra isolada
+ES - Espaços
 */
 
 //MAIS PALAVRAS
-//MELHORAR O DIFERENCIAL
 //COMENTAR
-//ADICIONAR A DICA
 //CRÉDITOS
-//REMOVER BATATA E ICONICO
+//FALAR SOBRE A DICA NA 7 PALAVRA
