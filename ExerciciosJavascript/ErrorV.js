6c0bd435694900b17a41b7830be336f702326eb5
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-08-25 12:27:32 -0300

    Update ErrorV.js

--- ExerciciosJavascript/ErrorV.js
+++ ExerciciosJavascript/ErrorV.js
@@ -16,3 +16,48 @@ for (IN = '', CH = 0; CH <= NL; CH++) {
     AE = NL - AE.length;
   }
 }
+
+for (IN = '', AC = 0; AC <= NL; AC++) {
+  if (AC != NL) {
+    if (IN.indexOf(PS.indexOf(PE.toLowerCase()[AC])) != -1) {
+      IN += PS.lastIndexOf(PE.toLowerCase()[AC]);
+    } else {
+      IN += PS.indexOf(PE.toLowerCase()[AC]);
+    }
+  } else {
+    IN = IN.replace(/-1/g,'');
+    for (AE = '', AC = 0; AC < IN.length; AC++) {
+      if (AE.indexOf(IN[AC]) != -1) {
+        continue;
+      }
+      AE += IN[AC];
+    }
+    AE = NL - AE.length;
+    AC = NL;
+  }
+}
+for (RS = false, RE = false, AC = 0; AC < NL; AC++) {
+  IN = PS.indexOf(PE.toLowerCase()[AC]);
+  OC = PS.lastIndexOf(PE.toLowerCase()[AC]);
+  if (IN != OC) {
+    RS = true;
+  }
+  FI = PE.toLowerCase().indexOf(PS[AC]);
+  LI = PE.toLowerCase().lastIndexOf(PS[AC]);
+  if (FI != LI) {
+    RE = true;
+  }
+  if (RS == true && RE == true) {
+    AE = LE;
+  }
+}
+(AE > LE) ? LE = AE: LE = LE; 
+
+/*
+var AE, AC, IN, RE, RS;
+AE - Algarismos errados
+AC - Analizador de caracteres
+IN - Indexes de letras
+RE - Repetições escolhida
+RS - Repetições sorteada
+*/
