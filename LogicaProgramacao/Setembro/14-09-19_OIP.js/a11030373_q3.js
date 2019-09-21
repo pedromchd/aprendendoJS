c23d38b29232643a7cc16f03765554d01d647acb
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-09-21 10:48:54 -0300

    Update a11030373_q3.js

--- LogicaProgramacao/Setembro/14-09-19_OIP.js/a11030373_q3.js
+++ LogicaProgramacao/Setembro/14-09-19_OIP.js/a11030373_q3.js
@@ -17,3 +17,25 @@ function main() {
   console.log(P);
   console.log(N);
 }
+
+function main() {
+  var H, N, P, Q;
+  H = parseInt(prompt());
+  N = parseInt(prompt());
+  Q = 0;
+  while (H >= 2) {
+    P = H * (H + H%2)/2 + Math.abs(H%2 - 1) * (H + H%2)/2;
+    (P <= N) ? N -= P + 0 * Q++ : H--;
+  }
+  console.log(Q);
+  console.log(N);
+}
+
+/*
+1 = 1 * 1
+2 = 2 * 1 + 1
+3 = 3 * 2
+4 = 4 * 2 + 2
+5 = 5 * 3
+6 = 6 * 3 + 3
+*/
