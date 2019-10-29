a97f4c7bc027277bdc23c65cae398f2622c61d55
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-10-28 21:06:48 -0300

    Update 21,28-10-19.js

--- LogicaProgramacao/Outubro/21,28-10-19.js
+++ LogicaProgramacao/Outubro/21,28-10-19.js
@@ -3,7 +3,7 @@ Antonella M. Cuello
 Pedro Garcia Machado
 Logica Prog. - 4oBim
 Profa Raquel Barbosa
-********************/
+*******************/
 
 //2048
 function sortNum() {
@@ -162,8 +162,8 @@ function WinOrLose (tabu) {
   var tab3_aux = tabu.join(',').split(',');
   for (C = 0; C < tab2_aux.length; C++) {
     if (tab2_aux[C] == '_') {
-	    tab2_aux.splice(C,1);
-		  tab2_aux.unshift('_');
+      tab2_aux.splice(C,1);
+      tab2_aux.unshift('_');
     }
   }
   if (tab1_aux.join('') == tab2_aux.join('')) {
@@ -174,9 +174,9 @@ function WinOrLose (tabu) {
     var tab1_aux = tabu.join('').replace(/,/g,'').split('');
     var tab2_aux = tabu.join('').replace(/,/g,'').split('');
     for (C = 0; C < tab2_aux.length; C++) {
-    	if (tab2_aux[C] == '_') {
-	    	tab2_aux.splice(C,1);
-		    tab2_aux.unshift('_');
+      if (tab2_aux[C] == '_') {
+        tab2_aux.splice(C,1);
+        tab2_aux.unshift('_');
       }
     }
     if (tab1_aux.join('') == tab2_aux.join('')) {
@@ -184,53 +184,69 @@ function WinOrLose (tabu) {
     }
   }
   for (var I = 0, L = 0; L < 4; L++) {
-  	for (var C = 0; C < 4; C++, I++) {
-	  	tabu[L][C] = tab3_aux[I];
+    for (var C = 0; C < 4; C++, I++) {
+      tabu[L][C] = tab3_aux[I];
     }
   }
 }
-var T, P, V;
-P = 0; 
-V = 0;
-T = [['_','_','_','_'],['_','_','_','_'],['_','_','_','_'],['_','_','_','_']];
-var menu = parseInt(prompt('𝑩𝒆𝒎-𝒗𝒊𝒏𝒅𝒐 𝒂𝒐 𝟐𝟎𝟒𝟖!   (◡‿◡✿)' + '\n▸ Digite 1 para ver as regras;' + '\n▸ Digite 2 para ver os controles;' + '\n▸ Digite 3 para jogar.'));
-switch (menu) {
-  Como jogar?' + '\n▸ Quando dois blocos de números iguais se chocam, eles viram um só com valor igual a soma dos dois;' + '\n▸ A cada movimento é acrescentado um bloco com valor 2 ou com valor 4 (menor probabilidade);' + '\n▸ Você ganha quando conseguir um bloco no valor de 2048 (por isso o nome).' + '\nE os controles?' + '\n▹ W ou 8 : ↑ (para CIMA);' + '\n▹ A ou 4 : ← (para a ESQUERDA);' + '\n▹ S ou 2 : ↓ (para BAIXO);' + '\n▹ D ou 6 : → (para a DIREITA).' + '\n▹ Digite R ou 0 para resetar o jogo.' + '\n▹ Digite V ou 5 para mudar a visualização do tabuleiro.'); // hmm, eu posso diminuir, é q eu gostei do fromato com as perguntas mas ae acho que teria que por num menu pra poder fazer isso, posso fazer
-renovaTab(T);
-renovaTab(T);
-do {		
-	do {
-		var moves = prompt('PONTUAÇÃO: ' + P + '\n' + mostraTab(T) + '\n' + '\Digite os números ou letras dos  respectivos movimentos: ').trim().toUpperCase();
-    if (!isNaN(moves)) {
-      moves = parseInt(moves);
+do {
+  var T, P, V;
+  P = 0; 
+  V = 0;
+  T = [['','','',''],['','','',''],['','','',''],['','','','']];
+  do {
+    var menu = parseInt(prompt('𝑩𝒆𝒎-𝒗𝒊𝒏𝒅𝒐 𝒂𝒐 𝟐𝟎𝟒𝟖!   (◡‿◡✿)' + '\n▸ Digite 1 para ver as regras;' + '\n▸ Digite 2 para ver os controles;' + '\n▸ Digite 3 para jogar.'));
+    switch (menu) {
+      case 1:
+        alert('Como jogar?   (。⌒∇⌒)。' + '\n▸ Use as setas do teclado para movimentar os blocos;' + '\n▸ Quando dois blocos de números iguais se chocam, eles viram um só com valor igual a soma dos dois;' + '\n▸ A cada movimento é acrescentado um bloco com valor 2 ou com valor 4 (menor probabilidade);' + '\n▸ Você ganha quando conseguir um bloco no valor de 2048 (por isso o nome).');
+        break;
+      case 2:
+        alert('E os controles?   。(⌒∇⌒。)' + '\n▹ W ou 8 : ↑ (para CIMA);' + '\n▹ A ou 4 : ← (para a ESQUERDA);' + '\n▹ S ou 2 : ↓ (para BAIXO);' + '\n▹ D ou 6 : → (para a DIREITA).' + '\n▹ Digite R ou 0 para resetar o jogo.' + '\n▹ Digite V ou 5 para mudar a visualização do tabuleiro.');
+        break;
+      case 3:
+        break;
     }
-    if (moves == 'V' || moves == 5) {
-      V = ++V%4;
-    }
-	} while (['W','A','S','D','R','V',8,4,2,6,0,5].indexOf(moves) == -1);
-  switch (moves) {
-    case 'W': case 8: 
-      moveCima(T);
-      break;
-    case 'A': case 4:
-      moveEsq(T);
-      break;
-    case 'S': case 2:
-      moveBaixo(T);
-      break;
-    case 'D': case 6: 
-      moveDir(T);
-      break;
-    case 'R': case 0:
-      P = 0;
-      T = [['_','_','_','_'],['_','_','_','_'],['_','_','_','_'],['_','_','_','_']];
-      renovaTab(T);
-  }
+  } while (menu != 3);
   renovaTab(T);
-} while (WinOrLose(T) == undefined);
-if (WinOrLose(T) == true) {
-	alert(mostraTab(T) + '\nVocê ganhou!');
-} 
-if (WinOrLose(T) == false) {
-	alert(mostraTab(T) + '\nVocê perdeu!');
-}
+  renovaTab(T);
+  do {		
+    do {
+      var moves = prompt('PONTUAÇÃO: ' + P + '\n' + mostraTab(T) + '\n' + '\Digite os números ou letras dos  respectivos movimentos: ').trim().toUpperCase();
+      if (!isNaN(moves)) {
+        moves = parseInt(moves);
+      }
+      if (moves == 'V' || moves == 5) {
+        V = ++V%4;
+      }
+    } while (['W','A','S','D','R','V',8 ,4 ,2 ,6 ,0 ,5].indexOf(moves) == -1);
+    switch (moves) {
+      case 'W': case 8: 
+        moveCima(T);
+        break;
+      case 'A': case 4:
+        moveEsq(T);
+        break;
+      case 'S': case 2:
+        moveBaixo(T);
+        break;
+      case 'D': case 6: 
+        moveDir(T);
+        break;
+      case 'R': case 0:
+        P = 0;
+        T = [['','','',''],['','','',''],['','','',''],['','','','']];
+        renovaTab(T);
+    }
+    renovaTab(T);
+  } while (WinOrLose(T) == undefined);
+  if (WinOrLose(T) == true) {
+    alert(mostraTab(T) + '\nVocê ganhou!   ヽ(⌒∇⌒)ﾉ');
+  } 
+  if (WinOrLose(T) == false) {
+    alert(mostraTab(T) + '\nVocê perdeu!   o(╥﹏╥)o');
+  }
+  do {
+    var PlayAgain = prompt('➮ Deseja jogar novamente? (/•ิ_•ิ)/" + "\n(Responda com S ou N)').toUpperCase();
+  } while (PlayAgain != 'S' && PlayAgain != 'N');
+} while (PlayAgain != 'N');
+alert("FIM DE JOGO! ヽ(⊙０⊙)ノ" + "\n" + "\nObrigada por jogar!" + "\n" + "\nCRIADO POR: Antonella Cuello e Pedro Machado  (つ▀¯▀)つ");
