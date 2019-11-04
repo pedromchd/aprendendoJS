8defd2c039c55d141bccd7644b6b54fc4c203d98
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-11-04 18:31:57 -0300

    Create 04-11-19.js

---
+++ LogicaProgramacao/Novembro/04-11-19.js
@@ -0,0 +1,29 @@
+/*
+TCA - projeto (4,0)
+3/12 >> Entrega e apresentação
+5/11 >> Envio da proposta
+19/11 >> Andamento
+
+**PROPOSTA:
+Arquivo de texto (nome, turma, etc;)
+- Nome do projeto. EXplicação...
+- O que se pretende fazer;
+
+Curso Técnico em Informática para Internet
+Lógica de Programção - 4o Bimestre
+Professora Raquel M. Barbosa
+
+Pedro Garcia Machado - Turma 1C
+
+JOGO DA MEMÓRIA
+
+O jogo da memória consiste em um tabuleiro de tamanho vairado, onde existe uma quantidade variada de pares de cartas que devem ser combinadas
+Estes pares são embaralhados aleatoriamente e sem o conehcimento do jogador, e cabe a ele encontrar os pares que se combinam, virando dois a cada turno,
+uma vez que encontrados, eles permanecem virados no jogos jogo, se não, eles são virados novamente.
+A quantidade de pares serão decididas com base na dificuldade escolhida pelo jogador no início da partida, sendo eles:
+- 6 pares, 12 cartas, tabuleiro 3x4;
+- 8 pares, 16 cartas, tabuleiro 4x4;
+- 10 pares, 20 cartas, tabueliro 3x5;
+Haverá opção de mudar o tema do jogador conforme sua vontade;
+O jogador não perde o jogo a menos que desista, a única maneira de ganhar o jogo é encontrando todos os pares embaralhados;
+*/
