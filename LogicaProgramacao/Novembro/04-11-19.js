4569d0259518646561ce4ff7a14473ab00fb6e4a
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-11-05 10:24:23 -0300

    Update 04-11-19.js

--- LogicaProgramacao/Novembro/04-11-19.js
+++ LogicaProgramacao/Novembro/04-11-19.js
@@ -26,4 +26,30 @@ A quantidade de pares serão decididas com base na dificuldade escolhida pelo jo
 - 10 pares, 20 cartas, tabueliro 3x5;
 Haverá opção de mudar o tema do jogador conforme sua vontade;
 O jogador não perde o jogo a menos que desista, a única maneira de ganhar o jogo é encontrando todos os pares embaralhados;
+
+
+PROPOSTA T.C.A. 
+“JOGO DA MEMÓRIA” 
+
+Conceito: 
+  O jogo da memória consiste por um tabuleiro, onde se encontram uma série de pares de objetos, imagens, caracteres, etc., 
+dispostos de uma maneira aleatória no tabuleiro, com seu conteúdo escondido do jogador. 
+Este em cada turno deve elencar duas peças, com o objetivo de formar os pares correspondentes. 
+  Ao virar duas peças e mostrar seu conteúdo ao jogador, uma vez que elas formarem um par, 
+elas são eliminadas, porém se forem diferentes estas se escondem novamente e o jogador escolhe outro par. 
+O jogador ganha ao encontrar todos os pares corretos. Não é possível perder, mas há a opção de desistir do jogo/partida. 
+
+Execução do jogo: 
+  Ao entrar no jogo, será apresentado um menu, com regras, créditos, sair, jogar, etc. 
+Terá também a opção de escolher entre 2 temas para o tabuleiro: emojis, ou somente caracteres especiais. 
+Após isso o jogador deve decidir a dificuldade/tabuleiro com que irá jogar, sendo elas: 
+
+FÁCIL: Tabuleiro 3x4, 12 peças (6 pares); 
+MÉDIO: Tabuleiro 4x5, 20 peças (10 pares); 
+DIFÍCIL: Tabuleiro 5x6, 30 peças (15 pares). 
+
+  Ao início do jogo, o tabuleiro será mostrado com todas as peças substituídas por um único caractere, e 
+o jogador irá utilizar de coordenadas (A1, E3, C7) para escolher cada objeto a revelar, e assim sucessivamente 
+até que desista ou ganhe. Caso a pessoa desista/ganhe o jogo, ela é levada aos créditos e após para o menu principal. 
+
 */
