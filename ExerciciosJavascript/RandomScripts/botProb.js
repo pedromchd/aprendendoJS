ae77ea8b008ce9ff02cedb0322fad063d0ab6290
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-11-15 13:29:39 -0300

    Update botProb.js

--- ExerciciosJavascript/RandomScripts/botProb.js
+++ ExerciciosJavascript/RandomScripts/botProb.js
@@ -1,11 +1,9 @@
 //BOT PROB
-function botProb() {
-  var len, arr, srt, pos;
-  len = Math.ceil(Math.random() * 10);
-  for (var I = 0, arr = []; I < len; I++) {
-    srt = Math.round(Math.random());
-    arr.push(srt);
-  }
-  pos = Math.floor(Math.random() * len);
-  return prb = Boolean(arr[pos]);
+var N = Math.ceil(Math.random() * 10);
+for (var I = 0, V = []; I < N; I++) {
+  var S = Math.round(Math.random());
+  V.push(S);
 }
+N = Math.floor(Math.random() * N);
+S = V[N];
+console.log(V,S);
