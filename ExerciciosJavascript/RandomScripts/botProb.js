85449f213a134af7cf43e22f59e3470e206550a4
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-11-14 22:31:35 -0300

    Create botProb.js

---
+++ ExerciciosJavascript/RandomScripts/botProb.js
@@ -0,0 +1,9 @@
+//BOT PROB
+var N = Math.ceil(Math.random() * 10);
+for (var I = 0, V = []; I < N; I++) {
+	var S = Math.round(Math.random());
+	V.push(S);
+}
+N = Math.floor(Math.random() * N);
+S = V[N];
+console.log(V,S);
