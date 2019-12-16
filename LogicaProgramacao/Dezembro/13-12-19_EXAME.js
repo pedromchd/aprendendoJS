ded2d9f890baa8d07ebb2f2c528068d314ec8c58
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2019-12-16 20:45:38 -0300

    Update 13-12-19_EXAME.js

--- LogicaProgramacao/Dezembro/13-12-19_EXAME.js
+++ LogicaProgramacao/Dezembro/13-12-19_EXAME.js
@@ -1,11 +1,31 @@
 //1//
-function verify() {
+function condition() {
   N = [], U = [], L = [];
   for (var I = 0, J = 65, K = 97; K <= 122; I++, J++, K++) {
-    N.push(I%10);
+    N.push(String(I%10));
     U.push(String.fromCharCode(J));
     L.push(String.fromCharCode(K));
   }
 }
-var P, N, U, L;
+function verify(C) {
+  var n, u, l;
+  (N.indexOf(C) != -1) ? n = true : n = false;
+  (U.indexOf(C) != -1) ? u = true : u = false;
+  (L.indexOf(C) != -1) ? l = true : l = false;
+  return n || u || l
+}
+var P, N, U, L, p;
 P = prompt('Insira uma senha:');
+p = false;
+condition();
+if (P.length >= 6 && P.length <= 16) {
+  p = true;
+  for (var J = 0; J < P.length; J++) {
+    if (!verify(P[J])) {
+      p = false;
+      break;
+    }
+  }
+}
+if (!p) alert('Senha Inválida!');
+else alert('Senha Válida!');
